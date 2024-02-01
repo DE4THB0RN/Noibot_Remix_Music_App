1992c03b103adeff574b710371e315c854d04126
de4thb0rn <pedroapferreirass@gmail.com>
2024-01-31 21:14:07 -0300

    Tá funfando lindinho
    Erros a serem corrigidos:
    -Aplicativo não é destruído corretamente,continua tocando musica
    -Mudar o controle de áudio dentro do app para um fragmento
    -Mudar a ui do playerView
    -A notificação antiga aparece quando pausa a música
    
    Já feito:
    -Notificação editada

---
+++ app/src/main/java/com/example/noibotremix/PlayerFragment.java
@@ -0,0 +1,24 @@
+package com.example.noibotremix;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class PlayerFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+}
--- app/src/main/java/com/example/noibotremix/PlayerService.java
+++ app/src/main/java/com/example/noibotremix/PlayerService.java
@@ -4,6 +4,7 @@ import static android.graphics.Color.rgb;
 
 import static androidx.media3.common.util.NotificationUtil.IMPORTANCE_HIGH;
 
+import android.app.Notification;
 import android.support.v4.media.session.PlaybackStateCompat;
 
 
@@ -18,6 +19,8 @@ import androidx.media3.ui.PlayerNotificationManager;
 public class PlayerService extends MediaSessionService {
 
     private MediaSession mediaSession;
+    private ExoPlayer exoplay;
+    private PlayerNotificationManager playerNotif;
 
     @Override
     public void onCreate()
@@ -27,12 +30,15 @@ public class PlayerService extends MediaSessionService {
         final String channelId = R.string.app_name + " Music Channel";
         final int notifId = 1111112;
 
-        ExoPlayer exoplay = new ExoPlayer.Builder(this)
+        //O player
+        //NÃO DELETA NÃO IMPORTA O QUE ACONTEÇA
+        exoplay = new ExoPlayer.Builder(this)
                 .setHandleAudioBecomingNoisy(true)
                 .build();
 
-
-
+        //Pra que servem audios?
+        //Atributos de audio
+        //Animal
         AudioAttributes audioAttributes = new AudioAttributes.Builder()
                 .setUsage(C.USAGE_MEDIA)
                 .setContentType(C.AUDIO_CONTENT_TYPE_MUSIC)
@@ -42,26 +48,25 @@ public class PlayerService extends MediaSessionService {
 
         //Coisas pra montar a notificação
 
-        PlayerNotificationManager playerNotif = new PlayerNotificationManager.Builder(this,notifId,channelId)
+        playerNotif = new PlayerNotificationManager.Builder(this,notifId,channelId)
                 .setChannelImportance(IMPORTANCE_HIGH)
-                .setNotificationListener(new PlayerNotificationManager.NotificationListener() {
-                    @Override
-                    public void onNotificationCancelled(int notificationId, boolean dismissedByUser) {
-                        PlayerNotificationManager.NotificationListener.super.onNotificationCancelled(notificationId, dismissedByUser);
-                    }
-                })
-                .setNextActionIconResourceId(R.drawable.ic_baseline_skip_next_24)
-                .setPreviousActionIconResourceId(R.drawable.ic_baseline_skip_previous_24)
-                .setPauseActionIconResourceId(R.drawable.ic_baseline_pause_circle_24)
-                .setPlayActionIconResourceId(R.drawable.ic_baseline_play_circle_24)
-                .setSmallIconResourceId(R.drawable.noivern_smallo)
+                .setNotificationListener(notifListener)
+                .setSmallIconResourceId(R.drawable.ic_noivern_notif)
+                .setChannelDescriptionResourceId(R.string.app_name)
+                .setChannelNameResourceId(R.string.app_name)
                 .build();
 
+        playerNotif.setPlayer(exoplay);
+        playerNotif.setPriority(NotificationCompat.PRIORITY_MAX);
+        playerNotif.setUseNextActionInCompactView(true);
+        playerNotif.setUsePreviousActionInCompactView(true);
+        playerNotif.setUsePlayPauseActions(true);
+        playerNotif.setUseRewindAction(false);
+        playerNotif.setUseFastForwardAction(false);
+        playerNotif.setUseStopAction(false);
         playerNotif.setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
         playerNotif.setColor(rgb(0,0,0));
         playerNotif.setColorized(true);
-        playerNotif.setPlayer(exoplay);
-
 
         //Aqui é criada a MediaSession
         mediaSession = new MediaSession.Builder(this, exoplay).build();
@@ -70,7 +75,12 @@ public class PlayerService extends MediaSessionService {
     @Override
     public void onDestroy()
     {
-        mediaSession.getPlayer().release();
+        stopForeground(true);
+        playerNotif.setPlayer(null);
+        playerNotif = null;
+        if (exoplay.isPlaying()) exoplay.stop();
+        exoplay.release();
+        exoplay = null;
         mediaSession.release();
         mediaSession = null;
         super.onDestroy();
@@ -83,7 +93,23 @@ public class PlayerService extends MediaSessionService {
         return mediaSession;
     }
 
+    private final PlayerNotificationManager.NotificationListener notifListener = new PlayerNotificationManager.NotificationListener() {
+        @Override
+        public void onNotificationCancelled(int notificationId, boolean dismissedByUser) {
+            PlayerNotificationManager.NotificationListener.super.onNotificationCancelled(notificationId, dismissedByUser);
+            stopForeground(true);
+            if (exoplay.isPlaying()) {
+                exoplay.stop();
+                exoplay.release();
+            }
+        }
 
+        @Override
+        public void onNotificationPosted(int notificationId, Notification notification, boolean ongoing) {
+            PlayerNotificationManager.NotificationListener.super.onNotificationPosted(notificationId, notification, ongoing);
+            startForeground(notificationId,notification);
+        }
+    };
 
 }
 
